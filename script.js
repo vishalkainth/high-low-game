fcb076004bfac7f934acfda08c20071150bfa7d0
Glitch (hello-webpage) <none>
2019-08-04 22:27:31 +0000

    🍈⛩ Checkpoint ./index.html:2480445/2202 ./script.js:2480445/5786 ./style.css:2480445/838 ./hello-world.html:2480445/1370

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// JavaScript File\
+var secretNumber = 0,
+	numberOfGuesses = 0;
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+function writeMessage(elementId, message, appendMessage) {
+	var elemToUpdate = document.getElementById(elementId);
+	if (appendMessage) {
+		elemToUpdate.innerHTML = elemToUpdate.innerHTML + message;
+	} else {
+		elemToUpdate.innerHTML = message;
+	}
+};
+
+function newGame() {
+	secretNumber = Math.floor(Math.random() * 100) + 1;
+	numberOfGuesses = 0;
+	writeMessage('historyList', '');
+}
+
+function guessInRange(guess) {
+	return (guess > 0 && guess < 101);
+}
+
+function userGuessed() {
+	var userGuessed = document.getElementById('userGuess').value;
+	var statusArea = document.getElementById('statusArea');
+	var historyList = document.getElementById('historyList');
+	if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
+		writeMessage('statusArea', '<p>Please enter a number 1-100 and press the Guess button.</p>');
+	} else if (userGuessed.indexOf('.') != -1) {
+		writeMessage('statusArea', '<p>Please enter a whole number 1-100 and press the Guess button.</p>');
+	} else {
+		numberOfGuesses++;
+		if (userGuessed == secretNumber) {
+			// Got it
+			writeMessage('statusArea', '<p>You got me in ' + numberOfGuesses +' guesses, I was thinking ' + secretNumber + '. Let\'s go again...</p>');
+			newGame();
+		} else if (userGuessed < secretNumber) {
+			writeMessage('statusArea', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
+		} else {
+			writeMessage('statusArea', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
+		}
+	}
+
+	document.getElementById('userGuess').value = '';	
+}
+
+window.onload = function() {
+	newGame();
+	document.getElementById('buttonArea').addEventListener('click', userGuessed);
+};
